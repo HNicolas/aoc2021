883da94c853f644a934abcc9e56a306203705762
Nicolas Hervé <nicolas.herve.pessac@gmail.com>
2021-12-02 15:14:35 +0100

    Update day1 solution

--- src/day1.rs
+++ src/day1.rs
@@ -1,27 +1,29 @@
-fn solve(depths: &Vec<u16>) -> u16 {
-    let mut current_depth = depths[0];
+/// (x(i) + ... + x(i-n)) - (x(i-1) + ... + x(i-n-1)) = x(i) - x(i-n-1)
+/// where n is the size of the window
+/// i - n - 1 >= 0, i >= n + 1
+fn solve(depths: &Vec<u16>, window_size: usize) -> u16 {
     let mut steps = 0;
 
-    for &depth in &depths[1..] {
-        if depth > current_depth {
+    for i in (1 + window_size)..depths.len() {
+        if depths[i] > depths[i - window_size - 1] {
             steps += 1;
         }
-        current_depth = depth;
     }
     steps
-}
-
-fn to_depths(lines: &Vec<&str>, windows_size: usize) -> Vec<u16> {
-    lines.windows(windows_size).map(|slice| slice.iter().fold(0, |acc, &curr| acc + curr.parse::<u16>().unwrap())).collect()
 }
 
 pub fn run() {
     let timer = std::time::Instant::now();
 
     let contents = std::fs::read_to_string("inputs/day1").unwrap();
-    let lines = contents.lines().collect::<Vec<_>>();
-    let solution1 = solve(&to_depths(&lines, 1));
-    let solution2 = solve(&to_depths(&lines, 3));
+    let depths = contents.lines().map(|s| s.parse().unwrap()).collect();
+    let solution1 = solve(&depths, 0);
+    let solution2 = solve(&depths, 2);
 
-    println!("solution 1 : {}, solution 2 : {}, {}us", solution1, solution2, timer.elapsed().as_micros());
+    println!(
+        "solution 1 : {}, solution 2 : {}, {}us",
+        solution1,
+        solution2,
+        timer.elapsed().as_micros()
+    );
 }
