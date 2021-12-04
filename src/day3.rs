796655991b9c65956e35615d6da1bda7aea565dc
Nicolas Hervé <nicolas.herve.pessac@gmail.com>
2021-12-04 20:04:30 +0100

    Add day3 part two

--- src/day3.rs
+++ src/day3.rs
@@ -20,13 +20,73 @@ fn solve1(contents: &str) -> u64 {
     gamma * epsilon
 }
 
+fn split(values: Vec<Vec<char>>, index: usize) -> (Vec<Vec<char>>, Vec<Vec<char>>) {
+    let mut ones = vec![];
+    let mut zeros = vec![];
+    for chars in values {
+        if chars[index] == '1' {
+            ones.push(chars);
+        } else {
+            zeros.push(chars);
+        }
+    }
+    (ones, zeros)
+}
+
+fn to_decimal(value: &Vec<char>) -> u64 {
+    let mut result = 0;
+    for i in 0..value.len() {
+        if value[i] == '1' {
+            result += 2u64.pow(11 - (i as u32));
+        }
+    }
+    result
+}
+
+fn solve2(contents: &str) -> u64 {
+    let values = contents
+        .lines()
+        .map(|s| s.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let (ones, zeros) = split(values, 0);
+    let (mut oxigen, mut co2) = if ones.len() >= zeros.len() {
+        (ones, zeros)
+    } else {
+        (zeros, ones)
+    };
+
+    let mut index = 1;
+    while oxigen.len() > 1 && index < 12 {
+        let (ones, zeros) = split(oxigen, index);
+        oxigen = if ones.len() >= zeros.len() {
+            ones
+        } else {
+            zeros
+        };
+        index += 1;
+    }
+
+    index = 1;
+    while co2.len() > 1 && index < 12 {
+        let (ones, zeros) = split(co2, index);
+        co2 = if zeros.len() <= ones.len() {
+            zeros
+        } else {
+            ones
+        };
+        index += 1;
+    }
+
+    to_decimal(&oxigen[0]) * to_decimal(&co2[0])
+}
+
 pub fn run() {
     let timer = std::time::Instant::now();
 
     let contents = std::fs::read_to_string("inputs/day3").unwrap();
 
     let solution1 = solve1(&contents);
-    let solution2 = "";
+    let solution2 = solve2(&contents);
 
     println!(
         "solution 1 : {}, solution 2 : {}, {}us",
